907dadba6374da08bf17fb65eda529681b11bc1f
asok3861 <asok3861@naver.com>
2023-10-09 22:26:50 +0900

    슬라이드 화살표 호버 시 버튼 보이는 기능추가

--- src/components/UI/Slide/ContentsSlide.js
+++ src/components/UI/Slide/ContentsSlide.js
@@ -12,6 +12,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
+import 'swiper/css/navigation';
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
